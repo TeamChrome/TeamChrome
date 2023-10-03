7b39eba2301fe0ddcda3670547753ec676799589
erikmendoza426 <erik.mendoza.426@my.csun.edu>
2023-10-03 15:29:11 -0700

    scheduler

---
+++ Scheduler.java
@@ -0,0 +1,20 @@
+public class Scheduler {
+    private roomsBooked //ArrayList of BOOKED rooms
+
+    //
+    private void checkRoomAvailability() {
+    }
+
+    //sends email confirmation to
+    private void sendConfirmation() {
+    }
+
+    //books requested room for Guest and updates roomsBooked ArrayList
+    public void bookRoom() {
+    }
+
+    //cancels requested booked room for Guest and updates roomsBooked ArrayList
+    public void cancelRoom() {
+    }
+
+}
