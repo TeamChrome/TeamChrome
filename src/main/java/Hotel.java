ab86e6e0974499661bd525092d6f9a9ada5ba16d
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2023-10-17 15:18:33 -0700

    Created skeleton for Hotel

---
+++ src/main/java/Hotel.java
@@ -0,0 +1,15 @@
+package main.java;
+
+import java.util.ArrayList;
+
+public class Hotel{
+    String hotelName;
+    //Scheduler scheduler
+    //ArrayList<Room> roomList
+    //ArrayList<Guest> guestList
+
+
+
+}
+
+
