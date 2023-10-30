40b577469e5a9c83302217781e44c29bda02ef1a
maxwell <maxwell.kozlov.595@my.csun.edu>
2023-10-30 14:21:20 -0700

    Begun work on room filtering on GUI

--- src/main/java/RoomSearchModel.java
+++ src/main/java/RoomSearchModel.java
@@ -1,5 +1,37 @@
 package main.java;
 
 public class RoomSearchModel {
-    String roomName;
+    String roomType;
+    Float costPerNight;
+    Integer roomNumber;
+
+    public RoomSearchModel(String roomType, Float costPerNight, Integer roomNumber) {
+        this.roomType = roomType;
+        this.costPerNight = costPerNight;
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public Float getCostPerNight() {
+        return costPerNight;
+    }
+
+    public void setCostPerNight(Float costPerNight) {
+        this.costPerNight = costPerNight;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
 }
--- src/main/java/RoomSearchTableController.java
+++ src/main/java/RoomSearchTableController.java
@@ -1,11 +1,89 @@
 package main.java;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-import javax.swing.text.TableView;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class RoomSearchTableController {
+public class RoomSearchTableController implements Initializable {
 
     @FXML
-    private TableView<RoomSearchModel>
+    private TableView<RoomSearchModel> roomSearchTable;
+
+    @FXML
+    private TableColumn<RoomSearchModel,String> roomTypeTableColumn;
+
+    @FXML
+    private TableColumn<RoomSearchModel, Float> roomCostTableColumn;
+
+    @FXML
+    private TableColumn<RoomSearchModel, Integer> roomNumberTableColumn;
+
+    @FXML
+    private RadioButton suiteButton;
+
+    @FXML
+    private RadioButton kingButton;
+
+    ObservableList<RoomSearchModel> roomSearchModelObservableList = FXCollections.observableArrayList();
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        //fetch data here
+
+        String[] sampleRoomTypes = {"SUITE","KING","KING","QUEEN"};
+        Float[] sampleRoomCosts = {100.0f,200.0f,300.0f,400.0f};
+        Integer[] sampleRoomNumbers = {100,101,201,202};
+        for(int i = 0; i < sampleRoomNumbers.length; i++){
+            roomSearchModelObservableList.add(new RoomSearchModel(sampleRoomTypes[i],sampleRoomCosts[i],sampleRoomNumbers[i]));
+        }
+
+        roomTypeTableColumn.setCellValueFactory(new PropertyValueFactory<>("roomType"));
+        roomCostTableColumn.setCellValueFactory(new PropertyValueFactory<>("costPerNight"));
+        roomNumberTableColumn.setCellValueFactory(new PropertyValueFactory<>("roomNumber"));
+
+        roomSearchTable.setItems(roomSearchModelObservableList);
+
+        FilteredList<RoomSearchModel> filteredData = new FilteredList<>(roomSearchModelObservableList, b -> true);
+        suiteButton.selectedProperty().addListener(((observableValue, aBoolean, t1) -> {
+            filteredData.setPredicate(roomSearchModel -> {
+
+                if(roomSearchModel.getRoomType().equals("SUITE")){
+                    return t1;
+                }
+
+                return false;
+            });
+        }));
+
+        kingButton.selectedProperty().addListener(((observableValue, aBoolean, t1) -> {
+            filteredData.setPredicate(roomSearchModel -> {
+
+                if(roomSearchModel.getRoomType().equals("KING")){
+                    return t1;
+                }
+
+                return false;
+            });
+        }));
+
+        SortedList<RoomSearchModel> sortedData = new SortedList<>(filteredData);
+        sortedData.comparatorProperty().bind(roomSearchTable.comparatorProperty());
+
+        roomSearchTable.setItems(sortedData);
+    }
+
+
+
 }
