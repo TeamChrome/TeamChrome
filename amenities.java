df904b5a91552efd1d89dfc9fc65d0b240086dbf
Jasonross6064 <jasonross6064@gmail.com>
2023-11-02 15:40:53 -0700

    a

--- amenities.java
+++ amenities.java
@@ -1,15 +1,47 @@
-import java.time.Clock;
-public class Amenities {
+public class Amenity {
     private String name;
-    private Open open;
+    private boolean state;
 
-    public Amenity() {
+    public Amenity(String name, boolean State) {
         this.name = name;
-        this.open = open;
+        this.state = state;
     }
 
-    public boolean isOpen() {
-        Clock clock = Clock.systemDefaultZone();
-        if (clock.instant )
+    public String getName() {
+        return name;
     }
+
+    private void setName() {
+        this.name = name;
+    }
+
+    public boolean getState() {
+        return state;
+    }
+
+    public void setState() {
+        this.state = state;
+    }
+
+    public void isOpen(Amenity amenity) {
+        if (amenity.getState() == false) {
+            System.out.println(amenity.getName() + " is closed.");
+        }
+        else {
+            System.out.println(amenity.getName() + " is open.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String name = "pool";
+        boolean state = false;
+
+        Amenity amen = new Amenity(name, state);
+
+        amen.getName();
+        amen.isOpen(amen);
+
+    }
+
 }
