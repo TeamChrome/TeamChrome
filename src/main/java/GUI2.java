cfdc6858eddccbdac3603eacce1276e0127abb4a
juliannledo <juliann.do.526@my.csun.edu>
2023-11-03 23:09:22 -0700

    Testing out GUI front page

--- src/main/java/GUI2.java
+++ src/main/java/GUI2.java
@@ -1,9 +1,12 @@
 package main.java;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GUI2 extends Application {
@@ -14,10 +17,23 @@ public class GUI2 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Group group = new Group();
-        Scene scene = new Scene(group, 600, 300);
+        Text text = new Text();
+        text.setFont(Font.font("Century Gothic", 30));
+        text.setX(225);
+        text.setY(30);
+
+        text.setText("Hotel CSUN");
+
+        Group root = new Group();
+
+        ObservableList list = root.getChildren();
+
+        list.add(text);
+
+        //Group group = new Group();
+        Scene scene = new Scene(root, 600, 300);
         scene.setFill(Color.BEIGE);
-        primaryStage.setTitle("Testing");
+        primaryStage.setTitle("Welcome to Hotel Sun");
         primaryStage.setScene(scene);
         primaryStage.show();
 
