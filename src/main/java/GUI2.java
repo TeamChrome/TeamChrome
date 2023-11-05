cfed3582888e803bc52f258efb74feb7a2d6aabe
juliannledo <juliann.do.526@my.csun.edu>
2023-11-04 19:22:33 -0700

    Welcome page with buttons for GUI

--- src/main/java/GUI2.java
+++ src/main/java/GUI2.java
@@ -1,40 +1,22 @@
 package main.java;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GUI2 extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Text text = new Text();
-        text.setFont(Font.font("Century Gothic", 30));
-        text.setX(225);
-        text.setY(30);
+    public void start(Stage primaryStage) throws Exception{
 
-        text.setText("Hotel CSUN");
-
-        Group root = new Group();
-
-        ObservableList list = root.getChildren();
-
-        list.add(text);
-
-        //Group group = new Group();
-        Scene scene = new Scene(root, 600, 300);
-        scene.setFill(Color.BEIGE);
+        Parent root = FXMLLoader.load(getClass().getResource("FrontPage.fxml"));
         primaryStage.setTitle("Welcome to Hotel Sun");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root,600, 300 ));
         primaryStage.show();
 
     }
