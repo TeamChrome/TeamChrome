2a9a3de5bb36c14752182eb4d2cfa759e35fc025
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2023-11-10 15:10:20 -0800

    Wrote basic runner outline

---
+++ src/main/java/Runner.java
@@ -0,0 +1,18 @@
+package src.main.java;
+
+import src.Scheduler;
+
+public class Runner {
+    Scheduler scheduler;
+//    GUI gui;
+
+    public Runner(){
+        this.scheduler = new Scheduler();
+        //this.gui = new GUI();
+
+    }
+
+    public static void main(String[] args){
+        //this.gui.run();
+    }
+}
